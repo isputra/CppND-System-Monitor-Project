ad8a7917ee44fbe39ad6b7b83be1dc29de6f1dae
Ikhsan Putra <ikhsan_sz@yahoo.com>
2019-11-25 21:43:01 +0100

    calculate delta utilization

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,17 +1,25 @@
+#include <thread>
 #include "processor.h"
 #include "linux_parser.h"
 
 // TODO: Return the aggregate CPU utilization
 float Processor::Utilization() {
     int idle=0, nonidle=0, i=0;
+    int prev_idle=0, prev_nonidle=0;
+    prev_cpus = LinuxParser::CpuUtilization();
+    std::this_thread::sleep_for(std::chrono::seconds(2));
     now_cpus = LinuxParser::CpuUtilization();
     std::vector<std::string>::iterator iter_cpu; 
     for(iter_cpu = now_cpus.begin(); iter_cpu != now_cpus.end(); iter_cpu++, i++) {
         if(i == 3 || i == 4) {
+            prev_idle += std::stoi(prev_cpus[i]);
             idle += std::stoi(*iter_cpu);
         } else {
+            prev_nonidle += std::stoi(prev_cpus[i]);
             nonidle += std::stoi(*iter_cpu);
         }
     }
+    idle = idle - prev_idle;
+    nonidle = nonidle - prev_nonidle;
     return (float) nonidle / (nonidle + idle);
 }
