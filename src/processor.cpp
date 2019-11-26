0b97684027bac171dd869d13dde1186c7c5ded09
Ikhsan Putra <ikhsan_sz@yahoo.com>
2019-11-26 14:31:34 +0100

    use namespace LinuxParser & enum CPUStates

--- src/processor.cpp
+++ src/processor.cpp
@@ -2,16 +2,18 @@
 #include "processor.h"
 #include "linux_parser.h"
 
+using namespace LinuxParser;
+
 // TODO: Return the aggregate CPU utilization
 float Processor::Utilization() {
     int idle=0, nonidle=0, i=0;
     int prev_idle=0, prev_nonidle=0;
-    prev_cpus = LinuxParser::CpuUtilization();
+    prev_cpus = CpuUtilization();
     std::this_thread::sleep_for(std::chrono::seconds(2));
-    now_cpus = LinuxParser::CpuUtilization();
+    now_cpus = CpuUtilization();
     std::vector<std::string>::iterator iter_cpu; 
     for(iter_cpu = now_cpus.begin(); iter_cpu != now_cpus.end(); iter_cpu++, i++) {
-        if(i == 3 || i == 4) {
+        if(i == CPUStates::kIdle_ || i == CPUStates::kIOwait_) {
             prev_idle += std::stoi(prev_cpus[i]);
             idle += std::stoi(*iter_cpu);
         } else {
