1ec72fc87fe2f18640e505cbade80713904c1660
Ikhsan Putra <ikhsan_sz@yahoo.com>
2019-11-27 18:45:03 +0100

    add Process contructor accepting pid

--- src/system.cpp
+++ src/system.cpp
@@ -18,7 +18,16 @@ using std::vector;
 Processor& System::Cpu() { return cpu_; }
 
 // TODO: Return a container composed of the system's processes
-vector<Process>& System::Processes() { return processes_; }
+vector<Process>& System::Processes() {
+    vector<int> pids(LinuxParser::Pids());
+    vector<int>::iterator iter_pids;
+    for (iter_pids = pids.begin(); iter_pids != pids.end(); iter_pids++) {
+        Process p(*iter_pids);
+        processes_.push_back(p);
+    }
+    // sort processess before returning them
+    return processes_;
+}
 
 // TODO: Return the system's kernel identifier (string)
 std::string System::Kernel() { return LinuxParser::Kernel(); }
